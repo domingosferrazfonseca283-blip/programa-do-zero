6ec2ed52a6f4d380d050554e50531d9a88a59b84
domingosferrazfonseca283-blip <domingosferrazfonseca283@gmail.com>
2026-09-20 21:52:20 +0100

    Adicionar import do alinhamento vertical dos botões

--- app/src/main/java/com/programadodzero/PracticeCodingActivity.kt
+++ app/src/main/java/com/programadodzero/PracticeCodingActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
